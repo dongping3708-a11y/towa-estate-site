c7816e9788d8c8e3469319c86db1e157f7463672
dongping3708-a11y <dongping3708@gmail.com>
2026-05-04 04:01:40 +0900

    Import global stylesheet correctly

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
+import './globals.css'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <head>
-        <link rel="stylesheet" href="/globals.css" />
-      </head>
       <body>{children}</body>
     </html>
   )
